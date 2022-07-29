402c6532d820a145eb35993f23f32a73558f1fe3
haithamassoli <haitham.b.assoli@gmail.com>
2022-07-29 22:16:03 +0300

    added web view

--- src/screens/subject/index.tsx
+++ src/screens/subject/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useLayoutEffect, useState } from "react";
 import { View, Text } from "react-native";
+import { WebView } from "react-native-webview";
 import subjects from "../../data/Subjects.json";
 import type { StackScreenProps } from "@react-navigation/stack";
 import type { Subject } from "../../types";
@@ -8,13 +9,15 @@ import type { SubjectsStackParamList } from "../../types/navigation";
 type Props = StackScreenProps<SubjectsStackParamList, "Subject">;
 
 const SubjectScreen = ({ navigation, route }: Props) => {
-  const [subject, setSubject] = useState<Subject | undefined>({} as Subject);
+  const [subject, setSubject] = useState({} as Subject);
 
   useEffect(() => {
     const currentSubject = subjects.find(
       (subject) => subject.id === route.params.areaId
     );
-    setSubject(currentSubject);
+    if (currentSubject) {
+      setSubject(currentSubject);
+    }
   }, [navigation]);
 
   useLayoutEffect(() => {
@@ -22,9 +25,15 @@ const SubjectScreen = ({ navigation, route }: Props) => {
   }, [navigation]);
 
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Text>{subject?.name}</Text>
       <Text>{subject?.name2}</Text>
+      <WebView
+        source={{
+          html: '<h1 style="color:red"><center>Hello world</center></h1><h1>Haitham</h1><p>Haitham</p>',
+        }}
+      />
+
       <Text>{subject?.subjectLink}</Text>
     </View>
   );

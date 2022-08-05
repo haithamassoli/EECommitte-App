db191b136b5869bfa25de7b459eb8fb75cd7d047
haithamassoli <haitham.b.assoli@gmail.com>
2022-08-05 14:02:03 +0300

    loading in screens

--- src/screens/subjects/subject/index.tsx
+++ src/screens/subjects/subject/index.tsx
@@ -1,28 +1,35 @@
 import { useContext, useEffect, useState } from "react";
-import { View, Text, Pressable, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import subjects from "@Src/data/Subjects";
 import type { StackScreenProps } from "@react-navigation/stack";
 import type { Subject } from "@Types/index";
 import type { SubjectsStackParamList } from "@Types/navigation";
 import Colors from "@GlobalStyle/Colors";
 import { ThemeContext } from "@Src/store/themeContext";
-import { rtlWebview, screenWidth } from "@Utils/Helper";
 import { WebDisplay } from "@Components/webDisplay";
 
 type Props = StackScreenProps<SubjectsStackParamList, "Subject">;
-
 const SubjectScreen = ({ navigation, route }: Props) => {
   const [subject, setSubject] = useState({} as Subject);
+  const [loading, setLoading] = useState(true);
   const { theme } = useContext(ThemeContext);
   const textColor =
     theme === "light" ? Colors.lightTextColor : Colors.darkTextColor;
   useEffect(() => {
+    setLoading(true);
     const currentSubject = subjects.find(
       (subject) => subject.id === route.params.areaId
     );
     if (currentSubject) {
       setSubject(currentSubject);
     }
+    setLoading(false);
   }, [route?.params?.areaId]);
 
   useEffect(() => {
@@ -31,71 +38,80 @@ const SubjectScreen = ({ navigation, route }: Props) => {
     });
   }, [subject?.name]);
 
+  if (loading) {
+    return (
+      <ActivityIndicator style={{ flex: 1 }} size="large" color={Colors.gray} />
+    );
+  }
+
   return (
-    <ScrollView
-      style={{
-        flex: 1,
-        paddingHorizontal: 12,
-        paddingVertical: 4,
-      }}
-    >
-      <Text
+    <>
+      {loading}
+      <ScrollView
         style={{
-          color: textColor,
-          fontSize: 24,
-          fontWeight: "bold",
-        }}
-      >
-        {subject?.name2}
-      </Text>
-      <View
-        style={{
-          marginTop: 16,
-        }}
-      >
-        {subject?.aboutSubject && <WebDisplay html={subject?.aboutSubject} />}
-      </View>
-      <Pressable
-        onPress={() =>
-          navigation.navigate("SubjectWebView", { url: subject?.subjectLink })
-        }
-        style={{
-          backgroundColor:
-            theme === "light" ? Colors.primary700 : Colors.primary400,
-          padding: 8,
-          borderRadius: 8,
-          marginVertical: 8,
+          flex: 1,
+          paddingHorizontal: 12,
+          paddingVertical: 4,
         }}
       >
         <Text
           style={{
-            color: Colors.dark,
+            color: textColor,
+            fontSize: 24,
+            fontWeight: "bold",
           }}
         >
-          Drive Link
+          {subject?.name2}
         </Text>
-      </Pressable>
-      <Pressable
-        onPress={() =>
-          navigation.navigate("SubjectFullPost", { post: subject?.fullPost })
-        }
-        style={{
-          backgroundColor:
-            theme === "light" ? Colors.primary400 : Colors.primary700,
-          padding: 8,
-          borderRadius: 8,
-          marginVertical: 8,
-        }}
-      >
-        <Text
+        <View
           style={{
-            color: Colors.dark,
+            marginTop: 16,
           }}
         >
-          Show Full Post
-        </Text>
-      </Pressable>
-    </ScrollView>
+          {subject?.aboutSubject && <WebDisplay html={subject?.aboutSubject} />}
+        </View>
+        <Pressable
+          onPress={() =>
+            navigation.navigate("SubjectWebView", { url: subject?.subjectLink })
+          }
+          style={{
+            backgroundColor:
+              theme === "light" ? Colors.primary700 : Colors.primary400,
+            padding: 8,
+            borderRadius: 8,
+            marginVertical: 8,
+          }}
+        >
+          <Text
+            style={{
+              color: Colors.dark,
+            }}
+          >
+            Drive Link
+          </Text>
+        </Pressable>
+        <Pressable
+          onPress={() =>
+            navigation.navigate("SubjectFullPost", { post: subject?.fullPost })
+          }
+          style={{
+            backgroundColor:
+              theme === "light" ? Colors.primary400 : Colors.primary700,
+            padding: 8,
+            borderRadius: 8,
+            marginVertical: 8,
+          }}
+        >
+          <Text
+            style={{
+              color: Colors.dark,
+            }}
+          >
+            Show Full Post
+          </Text>
+        </Pressable>
+      </ScrollView>
+    </>
   );
 };
 
--- src/screens/subjects/subjectWebView/index.tsx
+++ src/screens/subjects/subjectWebView/index.tsx
@@ -1,14 +1,16 @@
-import { Alert, Text, View } from "react-native";
+import { ActivityIndicator, Alert, Text, View } from "react-native";
 import { WebView } from "react-native-webview";
 import type { StackScreenProps } from "@react-navigation/stack";
 import type { SubjectsStackParamList } from "@Types/navigation";
 import { useEffect, useLayoutEffect, useState } from "react";
 import { isConnected } from "@Utils/Helper";
+import Colors from "@GlobalStyle/Colors";
 
 type Props = StackScreenProps<SubjectsStackParamList, "SubjectWebView">;
 
 const SubjectWebViewScreen = ({ navigation, route }: Props) => {
   const [isConnecte, setIsConnecte] = useState<boolean | null>(false);
+  const [loading, setLoading] = useState(true);
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: "درايف المادة",
@@ -21,42 +23,68 @@ const SubjectWebViewScreen = ({ navigation, route }: Props) => {
     });
   }, []);
 
+  // if (loading) {
+  //   return (
+  //     <ActivityIndicator style={{ flex: 1 }} size="large" color={Colors.gray} />
+  //   );
+  // }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        paddingVertical: 4,
-      }}
-    >
-      {isConnecte ? (
-        <WebView
-          showsVerticalScrollIndicator={false}
-          overScrollMode="never"
-          onError={(error) => {
-            Alert.alert(
-              "خطأ",
-              "حدث خطأ أثناء تحميل الموقع",
-              [{ text: "حسنا" }],
-              { cancelable: false }
-            );
-          }}
-          originWhitelist={["*"]}
-          source={{
-            uri: route.params.url,
-          }}
-        />
-      ) : (
-        <View
+    <>
+      {loading && (
+        <ActivityIndicator
           style={{
             flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            zIndex: 1,
+            margin: -20,
           }}
-        >
-          <Text>لا يوجد اتصال بالانترنت</Text>
-        </View>
+          size="large"
+          color={Colors.gray}
+        />
       )}
-    </View>
+
+      <View
+        style={{
+          flex: 1,
+          paddingVertical: 4,
+        }}
+      >
+        {isConnecte ? (
+          <WebView
+            showsVerticalScrollIndicator={false}
+            overScrollMode="never"
+            onError={(error) => {
+              Alert.alert(
+                "خطأ",
+                "حدث خطأ أثناء تحميل الموقع",
+                [{ text: "حسنا" }],
+                { cancelable: false }
+              );
+            }}
+            originWhitelist={["*"]}
+            onLoadEnd={() => {
+              setLoading(false);
+            }}
+            source={{
+              uri: route.params.url,
+            }}
+          />
+        ) : (
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text>لا يوجد اتصال بالانترنت</Text>
+          </View>
+        )}
+      </View>
+    </>
   );
 };
 

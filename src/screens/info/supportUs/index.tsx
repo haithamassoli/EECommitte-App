686c8f2ee84df01fe4d51c956dcd623025a5b12d
haithamassoli <haitham.b.assoli@gmail.com>
2022-08-06 12:30:46 +0300

    suppout us screen

--- src/screens/info/supportUs/index.tsx
+++ src/screens/info/supportUs/index.tsx
@@ -1,19 +1,19 @@
-import { View, Button } from "react-native";
+import { View, Button, Text } from "react-native";
 import { AdMobRewarded } from "expo-ads-admob";
 import { useEffect } from "react";
+import { Feather } from "@expo/vector-icons";
 
 const SupportUsScreen = () => {
-  
   useEffect(() => {
     return function cleanup() {
       AdMobRewarded.removeAllListeners();
     };
   }, []);
 
-  const loadAd = async() => {
+  const loadAd = async () => {
     await AdMobRewarded.setAdUnitID("ca-app-pub-6462207765068097/2318772812");
     await AdMobRewarded.requestAdAsync();
-  }
+  };
 
   AdMobRewarded.addEventListener("rewardedVideoUserDidEarnReward", (reward) => {
     console.log(reward);
@@ -26,12 +26,43 @@ const SupportUsScreen = () => {
 
   AdMobRewarded.addEventListener("rewardedVideoDidDismiss", () => {
     loadAd();
-  })
+  });
 
   loadAd();
   return (
-    <View style={{flex:1}}>
-      <Button title='Show Reward Ad' onPress={() => { AdMobRewarded.showAdAsync() }} />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button
+        title="Show Reward Ad"
+        onPress={() => {
+          AdMobRewarded.showAdAsync();
+        }}
+      />
+      <Text
+        style={{
+          fontFamily: "Bukra",
+          fontSize: 20,
+          textAlign: "center",
+          marginTop: 20,
+        }}
+      >
+        شكراً لوصولك هنا
+      </Text>
+      <Text
+        style={{
+          fontFamily: "TajawalRegular",
+          fontSize: 18,
+          textAlign: "center",
+          marginTop: 8,
+        }}
+      >
+        أشكرك لمجرد وصولك إلى هنا لدعمي أحتاج دعوتك الجميلة فقط :)
+      </Text>
+      <Feather
+        name="heart"
+        size={270}
+        color="#e74c3c"
+        style={{ marginTop: 20 }}
+      />
     </View>
   );
 };

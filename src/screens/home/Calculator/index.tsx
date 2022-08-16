0c765b182e7e69dc5b9a819f04247d3ee368bbe3
haithamassoli <haitham.b.assoli@gmail.com>
2022-08-16 17:46:33 +0300

    on press next slider

--- src/screens/home/Calculator/index.tsx
+++ src/screens/home/Calculator/index.tsx
@@ -1,19 +1,17 @@
 import { Feather } from "@expo/vector-icons";
 import { StackScreenProps } from "@react-navigation/stack";
 import { HomeStackParamList } from "@Types/navigation";
+import { screenHeight, screenWidth } from "@Utils/Helper";
 import { useLayoutEffect, useRef, useState } from "react";
 import {
   StatusBar,
   FlatList,
-  Image,
-  Animated,
   Text,
   View,
   Dimensions,
-  StyleSheet,
-  TouchableOpacity,
   ImageBackground,
   Pressable,
+  ScrollView,
 } from "react-native";
 
 type Props = StackScreenProps<HomeStackParamList, "Calculator">;
@@ -26,6 +24,7 @@ const data = [
     textEn1: "OUR IDEA WAS BORN 30 YEARS AGO!",
     textEn2: "AND IT CONTINUES AND WILL BE FOR EVER. AS LONG AS THE IDEA",
     image: require("@Assets/images/1stslide.png"),
+    arrPos: "right",
   },
   {
     textAr1:
@@ -37,23 +36,39 @@ const data = [
     textEn2:
       " IT’S ESTABLISHMENT DATES BACK TO BEFORE 1990, PARALLEL TO THE ESTABLISHMENT OF JUST.",
     image: require("@Assets/images/2ndslide.png"),
+    arrPos: "left",
   },
   {
     textAr1: "بدأت فكرتنا قبل أكثر من 30 عاماً!",
     textAr2: "وهي مستمرّة وستستمر طالما أن لها قلباً يجعلها حيّة..",
     textEn1: "OUR IDEA WAS BORN 30 YEARS AGO!",
-    textEn2: "                    HAS A HEART THAT MAKES IT ALIVE..",
+    textEn2: "AND IT CONTINUES AND WILL BE FOR EVER. AS LONG AS THE IDEA",
     image: require("@Assets/images/1stslide.png"),
+    arrPos: "right",
   },
 ];
 
-const imageW = width * 0.9;
-const imageH = imageW * 2;
-
 const CalculatorScreen = ({ navigation }: Props) => {
-  const scrollX = useRef(new Animated.Value(0)).current;
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const scrollRef = useRef<ScrollView>(null);
 
+  const onNext = () => {
+    setSelectedIndex((prevIndex) =>
+      prevIndex === data.length - 1 ? 0 : prevIndex + 1
+    ),
+      scrollRef.current?.scrollTo({
+        animated: true,
+        x: screenWidth * selectedIndex,
+        y: 0,
+      });
+  };
+
+  const setImageIndex = (event: any) => {
+    const contentOffset = event.nativeEvent.contentOffset;
+    const viewSize = event.nativeEvent.layoutMeasurement;
+    const newSelectedIndex = Math.floor(contentOffset.x / viewSize.width);
+    setSelectedIndex(newSelectedIndex);
+  };
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -74,134 +89,107 @@ const CalculatorScreen = ({ navigation }: Props) => {
   return (
     <View style={{ flex: 1, backgroundColor: "#000" }}>
       <StatusBar hidden />
-      <View style={StyleSheet.absoluteFillObject}>
-        {data.map((slider, index) => {
-          const inputRange = [
-            (index - 1) * width,
-            index * width,
-            (index + 1) * width,
-          ];
-          const opacity = scrollX.interpolate({
-            inputRange,
-            outputRange: [0, 1, 0],
-          });
-
-          return (
-            <Animated.Image
-              key={index}
-              source={slider.image}
-              style={[StyleSheet.absoluteFillObject, { opacity }]}
-              blurRadius={50}
-            />
-          );
-        })}
-      </View>
-      <Animated.FlatList
-        data={data}
-        keyExtractor={(_, index) => index.toString()}
-        onScroll={Animated.event(
-          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-          { useNativeDriver: true }
-        )}
+      <ScrollView
+        ref={scrollRef}
         horizontal
-        pagingEnabled
         showsHorizontalScrollIndicator={false}
-        renderItem={({ item, index }) => {
-          return (
-            <View
+        onMomentumScrollEnd={setImageIndex}
+        pagingEnabled
+      >
+        {data.map((item, index) => (
+          <View
+            key={index}
+            style={{
+              flex: 1,
+              width,
+              justifyContent: "center",
+              overflow: "hidden",
+              shadowRadius: 20,
+              shadowColor: "#000",
+              shadowOpacity: 0.5,
+              shadowOffset: {
+                width: 0,
+                height: 0,
+              },
+            }}
+          >
+            <ImageBackground
+              source={item.image}
               style={{
-                flex: 1,
-                width,
-                justifyContent: "center",
-                overflow: "hidden",
-                shadowRadius: 20,
-                shadowColor: "#000",
-                shadowOpacity: 0.5,
-                shadowOffset: {
-                  width: 0,
-                  height: 0,
-                },
+                width: screenWidth,
+                height: screenHeight,
+                alignSelf: "center",
               }}
             >
-              <ImageBackground
-                source={item.image}
+              <View
                 style={{
-                  width: imageW,
-                  height: imageH,
-                  alignSelf: "center",
+                  flex: 1,
+                  justifyContent: "flex-end",
+                  padding: 16,
                 }}
               >
-                <View
+                <Pressable
                   style={{
-                    flex: 1,
-                    justifyContent: "flex-end",
-                    padding: 16,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "#000",
+                    width: 50,
+                    height: 50,
+                    borderRadius: 25,
+                    top: -100,
+                    left: item.arrPos === "right" ? 40 : screenWidth - 80,
+                  }}
+                  onPress={onNext}
+                >
+                  <Feather name="arrow-right" size={32} color="#fff" />
+                </Pressable>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    marginTop: 8,
+                    fontFamily: "Bukra",
+                    top: -80,
                   }}
                 >
-                  <Pressable
-                    style={{
-                      justifyContent: "center",
-                      alignItems: "center",
-                      backgroundColor: "#000",
-                      width: 50,
-                      height: 50,
-                      borderRadius: 25,
-                      top: -100,
-                      left: 40,
-                    }}
-                    onPress={() => {}}
-                  >
-                    <Feather name="arrow-left" size={32} color="#fff" />
-                  </Pressable>
-
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      marginTop: 8,
-                      fontFamily: "Bukra",
-                      top: -80,
-                    }}
-                  >
-                    {item.textAr1}
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 14,
-                      marginTop: 8,
-                      fontFamily: "Bukra",
-                      top: -80,
-                      lineHeight: 20,
-                    }}
-                  >
-                    {item.textAr2}
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      marginTop: 8,
-                      fontWeight: "bold",
-                      top: -55,
-                    }}
-                  >
-                    {item.textEn1}
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 14,
-                      marginTop: 8,
-                      fontWeight: "600",
-                      lineHeight: 20,
-                      top: -55,
-                    }}
-                  >
-                    {item.textEn2}
-                  </Text>
-                </View>
-              </ImageBackground>
-            </View>
-          );
-        }}
-      />
+                  {item.textAr1}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    marginTop: 8,
+                    fontFamily: "Bukra",
+                    top: -80,
+                    lineHeight: 20,
+                  }}
+                >
+                  {item.textAr2}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    marginTop: 8,
+                    fontWeight: "bold",
+                    top: -55,
+                  }}
+                >
+                  {item.textEn1}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    marginTop: 8,
+                    fontWeight: "600",
+                    lineHeight: 20,
+                    top: -55,
+                  }}
+                >
+                  {item.textEn2}
+                </Text>
+              </View>
+            </ImageBackground>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };

489dec4a31854c547eb3b563df45471d2375ee49
haithamassoli <haitham.b.assoli@gmail.com>
2022-09-24 07:01:18 +0300

    edit plan image and screen

--- src/screens/subjects/plan/index.tsx
+++ src/screens/subjects/plan/index.tsx
@@ -2,15 +2,11 @@ import { StackScreenProps } from "@react-navigation/stack";
 import ReactNativeZoomableView from "@openspacelabs/react-native-zoomable-view/src/ReactNativeZoomableView";
 import ImageMapper from "@Components/imageMapper";
 import { screenWidth } from "@Utils/Helper";
-
 import type { MapperItem } from "@Types/index";
 import type { SubjectsStackParamList } from "@Types/navigation";
-import styles from "./styles";
 import MAPPING from "./Mapping";
-import { ThemeContext } from "@Src/store/themeContext";
-import { memo, useContext } from "react";
-import { Text, View } from "react-native";
-import Colors from "@GlobalStyle/Colors";
+import { memo } from "react";
+import { Image, StyleSheet, ImageBackground } from "react-native";
 import { horizontalScale, moderateScale, verticalScale } from "@Utils/Platform";
 
 type Props = StackScreenProps<SubjectsStackParamList, "Plan">;
@@ -19,41 +15,48 @@ const PlanScreen = ({ navigation }: Props) => {
   const handleSelectArea = (subjectId: number) => {
     navigation.navigate("Subject", { subjectId });
   };
-  const { theme } = useContext(ThemeContext);
   return (
-    <View style={styles.container}>
-      <Text
+    <>
+      <ImageBackground
+        source={require("@Assets/images/PlanBackground.png")}
         style={{
-          fontFamily: "Bukra",
-          fontSize: moderateScale(24),
-          color: theme === "light" ? Colors.gray : Colors.darkText,
-          alignSelf: "flex-start",
-          marginLeft: horizontalScale(20),
-          marginTop: verticalScale(20),
+          ...StyleSheet.absoluteFillObject,
         }}
+        resizeMode="cover"
       >
-        اختر مادة:
-      </Text>
-      <ReactNativeZoomableView
-        maxZoom={1.5}
-        minZoom={0.5}
-        zoomStep={0.5}
-        initialZoom={1}
-        bindToBorders={true}
-        style
-      >
-        <ImageMapper
-          imgSource={require("@Assets/images/plan.webp")}
-          imgWidth={screenWidth}
-          imgHeight={screenWidth * 1.074}
-          imgMap={MAPPING}
-          containerStyle={styles.container}
-          onPress={(item: MapperItem) => {
-            handleSelectArea(item.id);
-          }}
-        />
-      </ReactNativeZoomableView>
-    </View>
+        <ReactNativeZoomableView
+          maxZoom={1.5}
+          minZoom={0.5}
+          zoomStep={0.5}
+          initialZoom={1}
+          bindToBorders={true}
+        >
+          <ImageMapper
+            imgSource={require("@Assets/images/plan.png")}
+            imgWidth={screenWidth}
+            imgHeight={screenWidth * 0.97}
+            imgMap={MAPPING}
+            containerStyle={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            onPress={(item: MapperItem) => {
+              handleSelectArea(item.id);
+            }}
+          />
+          <Image
+            source={require("@Assets/images/planLowerbar.png")}
+            resizeMode="contain"
+            style={{
+              width: screenWidth,
+              height: screenWidth * 0.18,
+              bottom: verticalScale(30),
+            }}
+          />
+        </ReactNativeZoomableView>
+      </ImageBackground>
+    </>
   );
 };
 

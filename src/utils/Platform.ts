40e997b8caa28e5fc4688c49718d5f0ae4655223
haithamassoli <haitham.b.assoli@gmail.com>
2023-06-26 12:51:44 +0300

    responsive scale

--- src/utils/Platform.ts
+++ src/utils/Platform.ts
@@ -4,13 +4,23 @@ import { screenHeight, screenWidth } from "./Helper";
 const guidelineBaseWidth = 375;
 const guidelineBaseHeight = 812;
 
-const horizontalScale = (size: number) =>
-  (screenWidth / guidelineBaseWidth) * size;
-const verticalScale = (size: number) =>
-  (screenHeight / guidelineBaseHeight) * size;
-const moderateScale = (size: number, factor = 0.5) =>
-  size + (horizontalScale(size) - size) * factor;
+const [shortDimension, longDimension] =
+  screenWidth < screenHeight
+    ? [screenWidth, screenHeight]
+    : [screenHeight, screenWidth];
 
-export { horizontalScale, verticalScale, moderateScale };
+export const horizontalScale = (size: number) =>
+  (shortDimension / guidelineBaseWidth) * size;
+export const verticalScale = (size: number) =>
+  (longDimension / guidelineBaseHeight) * size;
+export const moderateScale = (size: number, factor = 0.5) =>
+  size + (horizontalScale(size) - size) * factor;
+export const moderateVerticalScale = (size: number, factor = 0.5) =>
+  size + (verticalScale(size) - size) * factor;
 
 export const isIOS = Platform.OS === "ios";
+
+export const hs = horizontalScale;
+export const vs = verticalScale;
+export const ms = moderateScale;
+export const mvs = moderateVerticalScale;

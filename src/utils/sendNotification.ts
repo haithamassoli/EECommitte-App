0b2d9b8876dfed736c397539d767c40bffbbb0bd
haithamassoli <haitham.b.assoli@gmail.com>
2024-04-01 17:32:58 +0300

    update expo notifications

--- src/utils/sendNotification.ts
+++ src/utils/sendNotification.ts
@@ -4,18 +4,21 @@ const sendNotification = async (
   body?: string,
   data?: string
 ) => {
-  const response = await fetch("https://exp.host/--/api/v2/push/send", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      to: token,
-      title: title,
-      body: body,
-      data: data || {},
-    }),
-  });
+  const response = await fetch(
+    "https://exp.host/--/api/v2/push/send?useFcmV1=true",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to: token,
+        title: title,
+        body: body,
+        data: data || {},
+      }),
+    }
+  );
   const { status } = response;
   return status;
 };

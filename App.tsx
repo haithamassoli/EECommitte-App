7159a6633afb08351f07fbcf1376adbb2ddf2486
haithamassoli <haitham.b.assoli@gmail.com>
2025-07-10 12:32:09 +0300

    Implement code changes to enhance functionality and improve performance

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import "react-native-gesture-handler";
-import { useCallback, useContext, useEffect, useRef, useState } from "react";
-import * as Notifications from "expo-notifications";
+import { useCallback, useContext, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Route from "./src/navigation/main";
@@ -23,6 +22,15 @@ import { FavoriteProvider } from "@Src/store/favoriteContext";
 import { setNotificationsTokens } from "@Src/api/setNotificationsTokens";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FlashList } from "@shopify/flash-list";
+import {
+  setNotificationHandler,
+  AndroidNotificationPriority,
+  AndroidImportance,
+  getExpoPushTokenAsync,
+  setNotificationChannelAsync,
+  requestPermissionsAsync,
+  getPermissionsAsync,
+} from "expo-notifications";
 import {
   PaperProvider,
   configureFonts,
@@ -51,11 +59,14 @@ const onFetchUpdateAsync = async () => {
   }
 };
 
-Notifications.setNotificationHandler({
+setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: true,
-    shouldSetBadge: true,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    priority: AndroidNotificationPriority.MAX,
+    shouldShowList: true,
   }),
 });
 
@@ -111,8 +122,6 @@ Text.defaultProps = Text.defaultProps || {};
 Text.defaultProps.allowFontScaling = false;
 
 export default function App() {
-  const responseListener = useRef();
-
   useEffect(() => {
     onFetchUpdateAsync();
     // deleteAllStorage();
@@ -134,73 +143,30 @@ export default function App() {
 
   useEffect(() => {
     const configurePushNotifications = async () => {
-      const { status } = await Notifications.getPermissionsAsync();
+      const { status } = await getPermissionsAsync();
       let finalStatus = status;
       if (finalStatus !== "granted") {
-        const { status } = await Notifications.requestPermissionsAsync();
+        const { status } = await requestPermissionsAsync();
         finalStatus = status;
       }
       if (finalStatus !== "granted") {
-        Alert.alert(
-          "الصلاحيات مطلوبة",
-          "يجب السماح بالإشعارات لتتمكن من استلامها"
-        );
         return;
       }
-      const pushNotificationsToken = await Notifications.getExpoPushTokenAsync({
+      const pushNotificationsToken = await getExpoPushTokenAsync({
         projectId: "17a53ebd-120e-477f-9b48-e87d75fd1a78",
       });
       setNotificationsTokens(pushNotificationsToken.data);
       // console.log(pushNotificationsToken.data);
 
       if (Platform.OS === "android") {
-        Notifications.setNotificationChannelAsync("default", {
+        setNotificationChannelAsync("default", {
           name: "default",
-          importance: Notifications.AndroidImportance.MAX,
+          importance: AndroidImportance.MAX,
           vibrationPattern: [0, 250, 250, 250],
         });
       }
     };
     configurePushNotifications();
-    // @ts-ignore
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((notification) => {
-        const addNotificationCount = () => {
-          const count = getDataMMKV("notificationsCount");
-          const notifications: any = getDataMMKV("notifications");
-          if (notifications) {
-            const newNotifications = [
-              {
-                title: notification.notification.request.content.title,
-                body: notification.notification.request.content.body,
-                link: notification.notification.request.content.data.link,
-                date: new Date(),
-              },
-              ...notifications,
-            ];
-            storeDataMMKV("notifications", newNotifications);
-          } else {
-            storeDataMMKV("notifications", [
-              {
-                title: notification.notification.request.content.title,
-                body: notification.notification.request.content.body,
-                link: notification.notification.request.content.data.link,
-                date: new Date(),
-              },
-            ]);
-          }
-          if (count != null) {
-            storeDataMMKV("notificationsCount", count + 1);
-          } else {
-            storeDataMMKV("notificationsCount", 1);
-          }
-        };
-        addNotificationCount();
-      });
-    return () => {
-      // @ts-ignore
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
   }, []);
 
   const onLayoutRootView = useCallback(async () => {
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,22 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo"]],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          unstable_transformImportMeta: true,
+          "react-compiler": {
+            sources: (filename) => {
+              // Match file names to include in the React Compiler.
+              return filename.includes("src/path/to/dir");
+            },
+            compilationMode: "strict",
+            panicThreshold: "all_errors",
+          },
+        },
+      ],
+    ],
     plugins: [
       [
         "module-resolver",
